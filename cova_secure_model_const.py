22df01313fcb646264ad62495a303b74223543d6
M Nazim Uddin <nazim.001.618@gmail.com>
2018-07-20 16:03:40 +0600

    bug fixed in cova_secure_model_const

--- cova_secure_model_const.py
+++ cova_secure_model_const.py
@@ -1,7 +1,6 @@
 MODEL_IMPORT_MAPS = {
     'ols' : {"import_name": 'linear_model', "model_name": '.LinearRegression', 
         "supervised": 1, "regression": 1},
-<<<<<<< HEAD
     'ridge': {"import_name": 'linear_model', "model_name": '.Ridge', 
         "supervised": 1, "regression": 1},
     'lasso': {"import_name": 'linear_model', "model_name": '.Lasso', 
@@ -53,64 +52,7 @@ MODEL_IMPORT_MAPS = {
 	'GaussianMixture' : {"import_name": 'mixture', "model_name": '.GaussianMixture', 
         "supervised": 0, "regression": 0},
 	'BayesianGaussianMixture' : {"import_name": 'mixture', "model_name": '.BayesianGaussianMixture', 
-        "supervised": 0, "regression": 0},
-=======
-    'ridge': 'linear_model',
-    'lasso': 'linear_model',
-    'elastic_net': 'linear_model',
-    'log_reg': 'linear_model',
-    'kernel_ridge': 'kernel_ridge',
-    'svc' : 'svm',
-    'svr' : 'svm',
-    'GaussianNB' : 'naive_bayes',
-    'BernoulliNB' : 'naive_bayes',
-    'MultinomialNB' : 'naive_bayes',
-	'dec_treeC' : 'tree',
-	'dec_treeR' : 'tree',
-	'rfc' : 'ensemble',
-	'rfr' : 'ensemble',
-	'MLPC' : 'neural_network',
-	'MLPR' : 'neural_network',
-	'KMeans' : 'cluster',
-	'MiniBatchKMeans' : 'cluster',
-	'AffinityPropagation' : 'cluster',
-	'MeanShift' : 'cluster',
-	'Spectral_Clustering' : 'cluster',
-	'Hierarchical_Clustering' : 'cluster',
-	'DBSCAN' : 'cluster',
-	'Birch' : 'cluster',
-	'GaussianMixture' : 'mixture',
-	'BayesianGaussianMixture' : 'mixture'
-}
-MODEL_IMPORT_MAPS2 = {
-    'ols' : '.LinearRegression',
-    'ridge': '.Ridge',
-    'lasso': '.Lasso',
-    'elastic_net': '.ElasticNet',
-    'log_reg': '.LogisticRegression',
-    'kernel_ridge': '.KernelRidge',
-    'svc' : '.SVC',
-    'svr' : '.SVR',
-    'GaussianNB' : '.GaussianNB',
-    'BernoulliNB' : '.BernoulliNB',
-    'MultinomialNB' : '.MultinomialNB',
-	'dec_treeC' : '.DecisionTreeClassifier',
-	'dec_treeR' : '.DecisionTreeRegressor',
-	'rfc' : '.RandomForestClassifier',
-	'rfr' : '.RandomForestRegressor',
-	'MLPC' : '.MLPClassifier',
-	'MLPR' : '.MLPRegressor',
-	'KMeans' : '.KMeans',
-	'MiniBatchKMeans' : '.MiniBatchKMeans',
-	'AffinityPropagation' : '.AffinityPropagation',
-	'MeanShift' : '.MeanShift',
-	'Spectral_Clustering' : '.SpectralClustering',
-	'Hierarchical_Clustering' : '.AgglomerativeClustering',
-	'DBSCAN' : '.DBSCAN',
-	'Birch' : '.Birch',
-	'GaussianMixture' : '.GaussianMixture',
-	'BayesianGaussianMixture' : '.BayesianGaussianMixture'
->>>>>>> 31fc26f70ef34e935eae9cd9bba6fcd3d680d7c5
+        "supervised": 0, "regression": 0}
 }
 S3_BUCKET_PATH = 's3/path/to/encrypted/dataset'
 DOCKER_VOLUME_PATH = '/home/docker_vol/'
